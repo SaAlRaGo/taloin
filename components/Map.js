001ecc8ea99b1022ece157f976d2df68ff7eb46d
= <antoniochavezmarco@gmail.com>
2024-07-28 16:35:29 -0600

    Creación de solicitudes completa

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity, Alert } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
 import tw from "tailwind-react-native-classnames";
@@ -13,14 +13,66 @@ import config from "../config.json";
 import { Modal, Pressable } from "react-native";
 import * as Location from "expo-location";
 import StarRating from "./StarRating";
+import * as ImagePicker from "expo-image-picker";
+import SelectedImage from "./SelectedImage";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
 const Map = () => {
   const mapRef = useRef(null);
+  const navigation = useNavigation();
   const { user, setUser } = useContext(UserContext);
   const { results, setResults } = useContext(ResultsContext);
   const [modalOpenned, setModalOpenned] = useState(false);
+  const [modalRequest, setModalRequest] = useState(false);
   const [workerView, setWorkerView] = useState({});
   const [region, setRegion] = useState({});
+  const [descripcion, setDescripcion] = useState("");
+  const [fotos, setFotos] = useState([]);
+  const [showDeleteImages, setShowDeleteImages] = useState({});
+  const toggleShowDeleteImage = (foto) => {
+      setShowDeleteImages(prevState => ({
+          ...prevState,
+          [foto]: !prevState[foto]
+      }));
+  };
 
+  const hideAllDeleteImages = () => {
+      const newShowDeleteImages = {};
+      fotos.forEach(foto => {
+          newShowDeleteImages[foto] = false;
+      });
+      setShowDeleteImages(newShowDeleteImages);
+  };
+  const pickImage = async () => {
+    // Solicita permisos para acceder a la biblioteca de imágenes
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      Alert.alert(
+        "Permiso para acceder a la biblioteca de imágenes es requerido."
+      );
+      return;
+    }
+
+    // Permite al usuario seleccionar una imagen
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      let uriParts = result.assets[0].uri.split(".");
+      let fileType = uriParts[uriParts.length - 1];
+      setFotos([...fotos, {
+        uri: result.assets[0].uri,
+        name: result.assets[0].fileName,
+        type: `image/${fileType}`,
+      }]);
+    }
+  };
   const getCurrentLocation = async () => {
     const response = { status: false, location: null };
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -63,6 +115,38 @@ const Map = () => {
       }
     });
   }, []);
+
+  const deleteFoto = (foto) => {
+    setFotos(fotos.filter((f) => f.uri !== foto));
+    setShowDeleteImages(prevState => {
+      const newState = { ...prevState };
+      delete newState[foto];
+      return newState;
+    });
+  }
+
+  const createRequest = async () => {
+    const formData = new FormData();
+    formData.append("description", descripcion);
+    formData.append("user_id", user.id);
+    formData.append("worker_id", workerView.id);
+    fotos.forEach((foto) => {
+      formData.append("photos", foto);
+    });
+    try {
+      const response = await axios.post(`${config.endpoint}/create/request`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if(response.data.id){
+        Alert.alert("Solicitud creada con éxito");
+        navigation.navigate("HomeScreen");
+      }
+    }catch (error) {
+      console.log(error);
+    }
+  }
 
   const styles = StyleSheet.create({
     map: {
@@ -127,7 +211,8 @@ const Map = () => {
       width: "90%",
       borderRadius: 20,
       padding: 20,
-      position: "relative"
+      position: "relative",
+      gap: 20,
     },
     btnCerrar: {
       position: "absolute",
@@ -151,8 +236,7 @@ const Map = () => {
       flexDirection: "row",
       alignItems: "center",
       justifyContent: "flex-start",
-      marginVertical: 10,
-      gap: 20,
+      gap: 10,
     },
     cardText: {
       fontSize: 16,
@@ -191,6 +275,28 @@ const Map = () => {
       fontSize: 18,
       fontWeight: "bold",
     },
+    input: {
+      minHeight: 50,
+      paddingHorizontal: 20,
+      borderColor: "#C319D2",
+      borderWidth: 1,
+      borderRadius: 7,
+    },
+    addPhoto: {
+      width: 30,
+      height: 30,
+      borderRadius: 15,
+      display: "flex",
+      alignContent: "center",
+      justifyContent: "center",
+      zIndex: 3
+    },
+    addPhotoImg:{
+      borderRadius: 5,
+      width: 40,
+      height: 40,
+      zIndex: 2
+    }
   });
   return (
     <>
@@ -259,9 +365,7 @@ const Map = () => {
           setModalOpenned(!modalOpenned);
         }}
       >
-        <View
-          style={modalStyles.fondo}
-        >
+        <View style={modalStyles.fondo}>
           <View style={modalStyles.modal}>
             <Pressable onPress={() => setModalOpenned(false)} style={modalStyles.btnCerrar}>
               <Image source={require("../assets/close.png")} style={modalStyles.btnCerrarImg}/>
@@ -282,13 +386,71 @@ const Map = () => {
                 </View>
               </View>
             </View>
-            <Pressable style={modalStyles.button}>
-              <Text style={modalStyles.buttonText}>Sí, sigamos</Text>
+            <Pressable style={modalStyles.button} onPress={() => {
+              setModalOpenned(false);
+              setModalRequest(true);
+            }}>
+              <Text style={modalStyles.buttonText}>Contactar trabajador</Text>
             </Pressable>
           </View>
         </View>
       </Modal>  
-      
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalRequest}
+        onRequestClose={() => {
+          setModalRequest(!modalRequest);
+        }}
+      >
+        <Pressable 
+          style={modalStyles.fondo}
+          onPress={hideAllDeleteImages}  
+        >
+          <View style={modalStyles.modal}>
+            <Pressable onPress={() => setModalRequest(false)} style={modalStyles.btnCerrar}>
+              <Image source={require("../assets/close.png")} style={modalStyles.btnCerrarImg}/>
+            </Pressable>
+            <View>
+              <Text style={modalStyles.tituloModal}>Solicitud de trabajo</Text>
+            </View>
+            <Text style={modalStyles.nombre}>Ingresa los detalles de tu problema:</Text>
+            <TextInput
+              style={modalStyles.input}
+              placeholder="Ingresa la descripción de tu problema"
+              value={descripcion}
+              onChangeText={setDescripcion}
+              multiline={true}
+              autoCorrect={false}
+              autoCapitalize={true}
+            />
+            <View style={modalStyles.card}>
+              {fotos.map((foto) => {
+                return (
+                  <SelectedImage
+                    key={foto.uri}
+                    foto={foto.uri}
+                    deleteFoto={deleteFoto}
+                    showDeleteImage={!!showDeleteImages[foto.uri]}
+                    toggleShowDeleteImage={() => toggleShowDeleteImage(foto.uri)}
+                  />
+                );
+              })}
+              <TouchableOpacity style={modalStyles.addPhoto} onPress={pickImage}>
+                <Image 
+                  source={require("../assets/addPhoto.png")}
+                  style={modalStyles.addPhotoImg}
+                />
+              </TouchableOpacity>
+            </View>
+            <Pressable style={modalStyles.button}
+              onPress={() => createRequest()}
+            >
+              <Text style={modalStyles.buttonText}>Completar solicitud</Text>
+            </Pressable>
+          </View>
+        </Pressable>
+      </Modal>
     </>
   );
 };
